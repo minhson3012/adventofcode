71d7b94e3721be333f337a6054366b9be6dc4f3c
Minh Son Nguyen <blackfox1688@gmail.com>
2018-12-06 11:54:26 +0700

    Updated part 2 of day 1

--- Day1/day1.py
+++ Day1/day1.py
@@ -1,10 +1,27 @@
-finalFreq = 0
+currentFreq = 0
+totalFreq = []
+duplicateFreq = 0
+duplicateFound = False
 
 f = open("day1input.txt", "r")
-for line in f:
-    value = int(line[1:])
-    if '+' in line:
-        finalFreq += value
-    else: 
-        finalFreq -= value
-print(finalFreq)
+lines = f.readlines()
+f.close()
+
+while True:
+    for line in lines:
+        value = int(line[1:])
+        sign = line[0]
+        if sign == '+':
+            currentFreq += value
+        else: 
+            currentFreq -= value
+
+        if currentFreq in totalFreq:
+            duplicateFreq = currentFreq
+            duplicateFound = not duplicateFound
+            break
+        else:
+            totalFreq.append(currentFreq)
+    if duplicateFound:
+        break
+print(duplicateFreq)
